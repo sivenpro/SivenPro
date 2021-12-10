f933f9160148ebc0bb4c1a13bb75161be800136e
sivenpro <90667375+sivenpro@users.noreply.github.com>
2021-12-10 18:04:06 -0300

    Delete sivenpro/main2/reportes directory

--- sivenpro/main2/reportes/index.php
+++
@@ -1,7 +0,0 @@
-<script src="js/core/jquery.min.js"></script>
-<script src="script.js"></script>
-<form action="" method="POST">
-    <p id="testButton"></p>
-    <input type="text" placeholder="Titulo" name="tituloPDF" />
-    <input type="button" id="submitPdf" name="submitPdf" value="crear PDF" name="generaPDF" />
-</form>
--- sivenpro/main2/reportes/printParte2.php
+++
@@ -1,7 +0,0 @@
-o
-<div id="cajas">
-    <h2>SEGUNDA PARTE</h2>
-    <div class="caja amarilla"></div>
-    <div class="caja azul"></div>
-</div>
-</body>
