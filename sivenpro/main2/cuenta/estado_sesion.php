7fce53143a3673989b5b489b012355c608877992
sivenpro <90667375+sivenpro@users.noreply.github.com>
2021-12-13 18:26:53 -0300

    Delete sivenpro/main2/cuenta directory

--- sivenpro/main2/cuenta/estado_sesion.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$estado = 'activo';
-if($_SESSION["interno_id"] == 0 && $_SESSION["interno_id"] == NULL){
-    $estado = 'desactivo';
-}
-echo json_encode( Array('respuesta' => $estado));
-?>
